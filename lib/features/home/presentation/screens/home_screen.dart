6f78e1215e51b3c3180496034223da80436c2eab
Mostafa Magdy Elsayed Ali <95254717+ProgrammerMostafa@users.noreply.github.com>
2023-05-23 12:29:02 +0300

    create custom appbar

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_books_app_clean_architecture/features/home/presentation/widgets/home_view_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
---
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_books_app_clean_architecture/features/home/presentation/widgets/custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+
+
